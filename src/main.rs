16ea10ea2bbc8d4a9d78081d78d64d9b0b902b80
Jay Howard <hello@jaydanhoward.com>
2023-07-29 12:12:04 -0700

    add email client

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
-
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
 
     let address = format!(
         "{}:{}",
@@ -22,5 +25,5 @@ async fn main() -> Result<(), std::io::Error> {
 
     println!("Started server on {}", &address);
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
