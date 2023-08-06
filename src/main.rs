f5bf8cd78999bfd76a655eae1c62d29a45d681ad
Jay Howard <hello@jaydanhoward.com>
2023-08-06 00:51:03 -0700

    add redis for session storage

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use zero2prod::configuration::get_configuration;
 use zero2prod::startup::Application;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> anyhow::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
 
     let application = Application::build(configuration).await?;
